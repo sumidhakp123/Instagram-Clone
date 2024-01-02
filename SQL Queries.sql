d6bebf0a0d9b579ac62f80faf70d93cd91fd25c6
sumidhakp123 <69155879+sumidhakp123@users.noreply.github.com>
2024-01-02 15:12:34 +0530

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -7,8 +7,8 @@ ORDER BY created_at
 LIMIT 5;
 
 -- q2 
-/*What day of the week do most users register on?
-We need to figure out when to schedule an ad campgain*/
+/* What day of the week do most users register on?
+We need to figure out when to schedule an ad campgain */
 SELECT date_format(created_at,'%W') AS 'day of the week', COUNT(*) AS 'total registration'
 FROM users
 GROUP BY 1
@@ -21,16 +21,16 @@ GROUP BY day
 ORDER BY total DESC;
 
 -- q3 
-/*We want to target our inactive users with an email campaign.
-Find the users who have never posted a photo*/
+/* We want to target our inactive users with an email campaign.
+Find the users who have never posted a photo */
 SELECT username
 FROM users
 LEFT JOIN photos ON users.id = photos.user_id
 WHERE photos.id IS NULL;
 
 -- q4 
-/*We're running a new contest to see who can get the most likes on a single photo.
-WHO WON??!!*/
+/* We're running a new contest to see who can get the most likes on a single photo.
+WHO WON??!! */
 SELECT users.username, photos.id, photos.image_url, COUNT(*) AS Total_Likes
 FROM likes
 JOIN photos ON photos.id = likes.photo_id
@@ -39,7 +39,7 @@ GROUP BY photos.id
 ORDER BY Total_Likes DESC
 LIMIT 1;
 
-/*Another Method*/
+/* Another Method */
 SELECT  username, photos.id, photos.image_url,  COUNT(*) AS total
 FROM photos
 INNER JOIN likes
@@ -51,13 +51,13 @@ ORDER BY total DESC
 LIMIT 1;
 
 -- q5 
-/*Our Investors want to know...
+/* Our Investors want to know...
 How many times does the average user post?
-/*total number of photos/total number of users*/
+/*total number of photos/total number of users */
 SELECT ROUND((SELECT COUNT(*)FROM photos)/(SELECT COUNT(*) FROM users),2);
 
 -- q6
-/* User ranking by postings higher to lower*/
+/* User ranking by postings higher to lower */
 SELECT users.username,COUNT(photos.image_url)
 FROM users
 JOIN photos ON users.id = photos.user_id
@@ -65,7 +65,7 @@ GROUP BY users.id
 ORDER BY 2 DESC;
 
 -- q7
-/*Total Posts by users. */
+/* Total Posts by users. */
 SELECT SUM(user_posts.total_posts_per_user)
 FROM (SELECT users.username,COUNT(photos.image_url) AS total_posts_per_user
 FROM users
@@ -73,14 +73,14 @@ JOIN photos ON users.id = photos.user_id
 GROUP BY users.id) AS user_posts;
 
 -- q8
-/*total numbers of users who have posted at least one time */
+/* Total numbers of users who have posted at least one time */
 SELECT COUNT(DISTINCT(users.id)) AS total_number_of_users_with_posts
 FROM users
 JOIN photos ON users.id = photos.user_id;
 
 -- q9
-/*A brand wants to know which hashtags to use in a post
-What are the top 5 most commonly used hashtags?*/
+/* A brand wants to know which hashtags to use in a post
+What are the top 5 most commonly used hashtags? */
 SELECT tag_name, COUNT(tag_name) AS total
 FROM tags
 JOIN photo_tags ON tags.id = photo_tags.tag_id
@@ -88,8 +88,8 @@ GROUP BY tags.id
 ORDER BY total DESC;
 
 -- q10
-/*We have a small problem with bots on our site...
-Find users who have liked every single photo on the site*/
+/* We have a small problem with bots on our site...
+Find users who have liked every single photo on the site */
 SELECT users.id,username, COUNT(users.id) As total_likes_by_user
 FROM users
 JOIN likes ON users.id = likes.user_id
@@ -97,8 +97,8 @@ GROUP BY users.id
 HAVING total_likes_by_user = (SELECT COUNT(*) FROM photos);
 
 -- q11
-/*We also have a problem with celebrities
-Find users who have never commented on a photo*/
+/* We also have a problem with celebrities
+Find users who have never commented on a photo */
 SELECT distinct username,comment_text
 FROM users
 LEFT JOIN comments ON users.id = comments.user_id
@@ -106,7 +106,7 @@ GROUP BY users.id
 HAVING comment_text IS NULL;
 
 -- q12
--- /* Find the count of users who have never commented on phto */  
+-- /* Find the count of users who have never commented on photo */  
 SELECT COUNT(*) FROM
 (SELECT username,comment_text
 FROM users
@@ -115,7 +115,7 @@ GROUP BY users.id
 HAVING comment_text IS NULL) AS total_number_of_users_without_comments;
 
 -- q13
-/*Find users who have ever commented on a photo*/
+/* Find users who have ever commented on a photo */
 SELECT username,comment_text
 FROM users
 LEFT JOIN comments ON users.id = comments.user_id
@@ -131,8 +131,8 @@ SELECT COUNT(*) FROM
 	HAVING comment_text IS NOT NULL) AS total_number_users_with_comments;
 
 -- q14
-/*Are we overrun with bots and celebrity accounts?
-Find the percentage of our users who have either never commented on a photo or have commented on photos before*/
+/* Are we overrun with bots and celebrity accounts?
+Find the percentage of our users who have either never commented on a photo or have commented on photos before */
     
     WITH UsersWithoutComments AS (
     SELECT username
