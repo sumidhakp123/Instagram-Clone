5afed52f9b6597707dd42efbe7b107cdde70fe20
sumidhakp123 <69155879+sumidhakp123@users.noreply.github.com>
2024-01-02 15:14:59 +0530

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -7,7 +7,7 @@ ORDER BY created_at
 LIMIT 5;
 
 -- q2 
-/* What day of the week do most users register on?
+/*What day of the week do most users register on?
 We need to figure out when to schedule an ad campgain*/
 SELECT date_format(created_at,'%W') AS 'day of the week', COUNT(*) AS 'total registration'
 FROM users
